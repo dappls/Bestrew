414602ae31c076d2996753c901efd2d390ed498e
dappls <101605544+dappls@users.noreply.github.com>
2025-12-02 16:56:55 -0800

    Added DataGen

---
+++ src/client/java/net/dappls/BestrewDataGenerator.java
@@ -0,0 +1,11 @@
+package net.dappls;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BestrewDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+    }
+
+}
